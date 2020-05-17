e610bdcdcdb9a70e2fbc1f3fc529bfff11ea4d11
Sakshi Srivastava <sakshidinesh@gmail.com>
2020-05-18 02:46:40 +0530

    basic login/signup template

--- src/app/components/authorization/authorization.module.ts
+++ src/app/components/authorization/authorization.module.ts
@@ -2,8 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
-
-
+import { AuthorizationRoutingModule } from './authorization-routing.module';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -11,7 +15,14 @@ import { SignupComponent } from './signup/signup.component';
     SignupComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    AuthorizationRoutingModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class AuthorizationModule {
--- src/app/components/authorization/login/login.component.ts
+++ src/app/components/authorization/login/login.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.sass']
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
 
+  public user: User  = {username: '', password: '', email: ''}
   constructor() { }
 
   ngOnInit(): void {
