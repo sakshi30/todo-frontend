455444aeaf0945ba5016de6aea0de194b2720fff
shardul <shardullingwal94@gmail.com>
2020-05-21 23:25:22 +0530

    displaying tasks

--- src/app/components/todo-list/todo-list-router.module.ts
+++ src/app/components/todo-list/todo-list-router.module.ts
@@ -2,11 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UpcomingComponent } from './upcoming/upcoming.component';
 import { ArchiveComponent } from './archive/archive.component';
-<<<<<<< HEAD
-import { HomeComponent } from './home/home.component';
-=======
 import { LoginGuard } from 'src/app/shared/login.guard';
->>>>>>> a7e891b72e593d25880aeb0bce1c3b4ae2b3f1af
 
 const routes: Routes = [{
     path: 'upcoming',
@@ -15,19 +11,9 @@ const routes: Routes = [{
 },
 {
     path: 'archive',
-<<<<<<< HEAD
-    component: ArchiveComponent
-},
-{
-  path: 'home',
-  component: HomeComponent
-}  
-];
-=======
     component: ArchiveComponent,
     canActivate: [LoginGuard]
 }];
->>>>>>> a7e891b72e593d25880aeb0bce1c3b4ae2b3f1af
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
